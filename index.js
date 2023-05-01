4926ff3024b40df6fb296966601e2d14e6df178d
Re-zan <zannat881all@gmail.com>
2023-05-01 21:53:39 +0600

    the-dragon-news server set up done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+var cors = require("cors");
+app.use(cors());
+const categories = require("./data/categories.json");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
