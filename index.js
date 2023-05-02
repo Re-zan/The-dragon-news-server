34d6093b446db8100d5bca3d3dff1932508dae31
Re-zan <zannat881all@gmail.com>
2023-05-02 12:57:30 +0600

    new json file create

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000;
 var cors = require("cors");
 app.use(cors());
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -11,6 +12,28 @@ app.get("/", (req, res) => {
 
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const newsDetails = news.find((newsData) => newsData._id === id);
+  res.send(newsDetails);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const catgoryTypeData = news.filter(
+      (nD) => parseInt(nD.category_id) === id
+    );
+    res.send(catgoryTypeData);
+  }
 });
 
 app.listen(port, () => {
